b4a0048f4a3ceb45067a689899afc8d85ddaf140
Alejandro Jiménez Agudo <a.j.agudo@gmail.com>
2017-08-20 20:08:25 +0200

    - Added cron demo

--- AppliveryDemo/ViewController.swift
+++ AppliveryDemo/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var timer: Timer!
+	var time = 0.0
+	@IBOutlet weak var labelTime: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(timerEvent), userInfo: nil, repeats: true)
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
 	}
+	
+	override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+		if motion == .motionShake {
+			NotificationCenter.default.post(name: NSNotification.Name.UIApplicationUserDidTakeScreenshot, object: nil)
+		}
+	}
+	
+	func timerEvent() {
+		self.time += 0.1
+		self.labelTime.text = "Time: \(self.time.f1())"
+	}
+	
+}
 
 
+extension Double {
+	func f1() -> String {
+		return self.format(".1")
+	}
+	
+	func format(_ format: String) -> String {
+		return String(format: "%\(format)f", self)
+	}
 }
 
